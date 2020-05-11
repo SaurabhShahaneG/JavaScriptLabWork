c1a3ed479fea6c0a9906e52713ff1b3b0d9e5f81
SaurabhShahaneG <65170210+SaurabhShahaneG@users.noreply.github.com>
2020-05-11 18:05:53 +0530

    Add files via upload

---
+++ highestNumber.js
@@ -0,0 +1,17 @@
+function highestNumber(num1, num2, num3) {
+    if (num1 > num2) {
+        if (num1 > num3) {
+            return num1;
+        } else {
+            return num3;
+        }
+    } else {
+        if (num2 > num3) {
+            return num2;
+        } else {
+            return num3;
+        }
+    }
+}
+
+console.log(highestNumber(8, 5, 4));
